9b2a9880463f0a77229dabc5982603f0840b55d2
BinScholl <BinScholl@foxmail.com>
2019-12-08 19:14:11 +0800

    添加一个测试题 计算一个字符串里面包含了几个中文字符📚

---
+++ lest_temp/for_test/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+//计算一个字符串里面包含有多少个中文字符
+func main() {
+	var str string = "Hello世界!"
+	StrArray := []rune(str)
+	count := 0
+	for _, c := range StrArray {
+		if len(string(c)) == 3 {
+			count++
+		}
+	}
+	fmt.Println("str里面中文字符的长度是:", count)
+}
