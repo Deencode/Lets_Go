a03d85485c968800e403a4f6dec353092704f776
Higker <deen.job@qq.com>
2020-04-12 16:53:39 +0800

    push go file writer example

--- lets_19_file_writer/lets_19_file_Writer.go
+++ lets_19_file_writer/lets_19_file_Writer.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
@@ -11,6 +13,11 @@ const (
 
 //go语言中文件写入操作
 func main() {
+	wr1()
+	wr2()
+	wr3()
+}
+func wr1() {
 	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0777)
 	if err != nil {
 		fmt.Println("文件打开失败~~")
@@ -18,4 +25,18 @@ func main() {
 	defer file.Close()
 	file.Write([]byte("岳宁宁在Google云计算部门,她负责的是Google Cloud CDN产品!"))
 	file.WriteString("\n而我却是给菜鸟~~sorry!")
+}
+func wr2() {
+	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0777)
+	if err != nil {
+		fmt.Println("文件打开失败~~")
+	}
+	defer file.Close()
+	wr := bufio.NewWriter(file)
+	wr.WriteString("\nYNN哈哈哈哈")
+	wr.Flush() //bufio在缓存里面 必须调用这个
+}
+func wr3() {
+	content := "不知道会不会有人看到这个内容哈哈哈哈哈~^_^"
+	ioutil.WriteFile(filePath, []byte(content), 0777)
 }
