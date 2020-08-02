0fb5f5e3efc2b9e6d5c5c953e8a637e914813f01
Dings <deen.job@qq.com>
2020-08-02 22:16:45 +0800

    add file

--- lets_50_web_framework/gin/server.go
+++ lets_50_web_framework/gin/server.go
@@ -11,12 +11,21 @@ import (
 func main() {
 	// 获取一个默认路由 router
 	r := gin.Default()
+	// curl http://localhost:8080/
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"os":         runtime.GOOS,
 			"go-version": runtime.Version(),
 			"now_time":   time.Now().Format("2006-01-02 15:04:05"),
 		})
+	})
+	// curl http://localhost:8080/user/ding/222
+	r.GET("/user/:name/*action", func(c *gin.Context) {
+		c.String(200, c.Param("name")+" / "+c.ClientIP())
+	})
+	// curl -i http://localhost:8080/user/ding\?id\=999
+	r.GET("/user/:name", func(c *gin.Context) {
+		c.String(200, c.Param("name")+" / ID="+c.DefaultQuery("id", "1"))
 	})
 	//fmt.Println(runtime.GOOS)
 	r.Run(":8080")
